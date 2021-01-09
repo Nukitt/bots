2d819b1532f2ac273b2533e0149073187c0d155c
Nukitt <nukit.tailor@students.iiit.ac.in>
2021-01-09 21:54:46 +0530

    add spam bot

---
+++ simple-spam-bot.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+i=0
+time.sleep(5)
+while i<200:
+    pyautogui.typewrite("OHHH MAAH GOOOO TRUUUU LOOOBB")
+    pyautogui.press("enter")
+    time.sleep(0.9)
+    i=i+1
